cfc7215962f16f5320cdc7d4b65f523d47980597
astyrrian1 <github@astyrrian.com>
2026-02-28 19:09:50 -0600

    fix: Pipe scenario data to native STDIN to trigger ESPHome EOF loop termination

--- run_cpp.sh
+++ run_cpp.sh
@@ -6,4 +6,4 @@ esphome compile tests_scenarios.yaml
 
 echo "Executing Native Binary..."
 rm -f cpp_scenarios.txt
-./.esphome/build/hapsic-scenarios/.pioenvs/hapsic-scenarios/program > cpp_scenarios.txt 2>&1
+cat simulation_data_sorted.csv | ./.esphome/build/hapsic-scenarios/.pioenvs/hapsic-scenarios/program > cpp_scenarios.txt 2>&1
